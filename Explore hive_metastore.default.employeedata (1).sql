85083ff9fa30fa887843482bd530a6fd555a38e7
thkchva <thkchva>
2024-05-09 09:37:04 +0000

    commit & push

--- Explore hive_metastore.default.employeedata (1).sql
+++ Explore hive_metastore.default.employeedata (1).sql
@@ -4,3 +4,8 @@ SELECT * FROM `hive_metastore`.`default`.`employeedata`;
 -- COMMAND ----------
 
 SELECT empname, AVG(salary) AS avgsalary FROM employeedata GROUP BY empname;
+
+-- COMMAND ----------
+
+SELECT empname 
+FROM employeedata WHERE country = 'china';
